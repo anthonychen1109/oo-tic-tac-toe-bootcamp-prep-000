52b09cc38f34314e4696aaa6301e02d9b109d81b
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-13 20:41:19 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,37 @@
 class TicTacToe
   
   def initialize(board)
-    @board = board
+    @board = [" "," "," "," "," "," "," "," "," "]
   end
+  
+  def display_board
+    "#{@board}"
+  end
+  
+  def input_to_index(input)
+    @board[input-1]
+  end
+  
+  def move(index, token="X")
+    @index = index
+    @token = token
+  end
+  
+  def position_taken
+    
+  end
+  
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+  
+  
   
 end
